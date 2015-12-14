9460c3897e64356f9cc9af5d801a621d2a0dae33
Mirabai <mirabai.applegate@gmail.com>
2015-12-13 18:02:53 -0800

    Refactor and get lightbox calls working with Instagram API

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,5 +1,27 @@
+// Begin process to open lightbox
 function openLightbox(userID) {
+  // Retrieve token
   var accessToken = instaAuth();
 
-  fetchFromInsta(userID, accessToken, "drawLightbox");
+  // Fetch images from Instagram
+  fetchFromInsta(userID, accessToken, "instaCallback");
+}
+
+// JSONP Instagram callback
+function instaCallback(data) {
+  var images = convertInstaData(data.data);
+  lightbox(images);
+}
+
+// Convert raw Instagram data to lightbox usable data
+function convertInstaData(data) {
+  var images = [];
+  for (i = 0; i < data.length; i++) {
+    var image = {};
+    var imageData = data[i];
+    image.caption = imageData.caption.text;
+    image.url = imageData.images.standard_resolution.url;
+    images.push(image);
+  }
+  return images;
 }
